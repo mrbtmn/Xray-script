32ccc55e733d070d46147ef48ab17410e5316efe
ARASH <103778049+mrbtmn@users.noreply.github.com>
2024-05-12 22:43:05 +0330

    Update Xray-TLS+Web-setup.sh

--- Xray-TLS+Web-setup.sh
+++ Xray-TLS+Web-setup.sh
@@ -24,7 +24,7 @@ country_name=$(curl https://ifconfig.net/country)
 
 #GetCityName
 #city_name=$(curl -s https://ipinfo.io/city)
-city_name=$(curl ifconfig.co/city)
+city_name=$(curl ifconfig.net/city)
 
 #GetIP
 serverIP=$(curl -s ipv4.ip.sb)
